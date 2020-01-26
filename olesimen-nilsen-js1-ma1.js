39ebbe4da333f9c08658abdadee5899fe4370395
Ole Simen Nilsen <olesimen.nilsen@gmail.com>
2020-01-26 14:48:26 +0100

    Question 1

--- olesimen-nilsen-js1-ma1.js
+++ olesimen-nilsen-js1-ma1.js
@@ -4,6 +4,12 @@ Create an object called cat.
 Give the object one property called complain. complain's value should be a method (a function) which logs the string "Meow!".
 */
 
+const cat = {
+    complain: function() {
+        console.log("Meow!");
+    }
+};
+
 /* Question 2
 Select the h3 from the HTML below using the querySelector method and assign it to a variable called heading.
 
