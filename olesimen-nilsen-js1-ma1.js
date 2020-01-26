09e4cefcc38b46b235bfab8d10b79ddc7c30a71d
Ole Simen Nilsen <olesimen.nilsen@gmail.com>
2020-01-26 19:23:47 +0100

    Questions 4-7

--- olesimen-nilsen-js1-ma1.js
+++ olesimen-nilsen-js1-ma1.js
@@ -29,9 +29,13 @@ heading.style.fontSize = "2em";
 Add a class to the heading variable called subheading.
 */
 
+heading.classList.add("subheading");
+
 /* Question 5
 Write code that selects all the p elements on a page and assigns them to a variable called paragraphs.
 */
+
+const paragraphs = document.querySelectorAll("p");
 
 /* Question 6
 Select the div by its class from the HTML below, assign it to a variable called resultsContainer and set its inner HTML to be <p>New paragraph</p>.
@@ -39,27 +43,41 @@ Select the div by its class from the HTML below, assign it to a variable called 
 <div class="results"></div>
 */
 
+const resultsContainer = document.querySelector(".results");
+resultsContainer.innerHTML = "<p>New paragraph</p>";
+
 /* Question 7
 Create a function that has one argument called catArray.
 
 Inside the function, loop through the catArray argument and console log the name property in each object.
 
 Call the function and pass in the cats variable below.
+*/
 
+// REMOVE THIS!!!!
 const cats = [
     {
         name: "Blob",
         age: 10
     },
     {
-        name: "Harold",
+        name: "Harold"
     },
     {
         name: "Blurt",
         age: 21
     }
 ];
-*/
+
+// REMOVE THIS !!!!
+
+function catName(catArray) {
+    for (let i = 0; i < catArray.length; i++) {
+        console.log(catArray[i].name);
+    }
+}
+
+catName(cats);
 
 /* Question 8
 Using the function and cats variable from the above question, instead of logging the name property, wrap each name property in an h5 tag, add it to a variable you declare before the loop and return the variable from the function after the loop.
@@ -69,6 +87,11 @@ The function should return the following:
 <h5>Blob</h5>
 <h5>Harold</h5>
 <h5>Blurt</h5>
+/*
+
+
+
+/*
 Question 9
 Call the function from question 8, pass it the cats variable from question 7 and set the inner HTML of the resultsContainer variable from question 6 to the return value of the function.
 */
